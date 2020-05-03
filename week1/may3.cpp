ba77097e95736a2148f9678f942f3dc6c872d1e3
Rishabh <rishabh.kumar@medlife.com>
2020-05-03 19:17:09 +0530

    may3 solution

---
+++ week1/may3.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> occ(26, 0);
+        for(char c: magazine) occ[c-'a']++;
+        for(char c: ransomNote){
+            occ[c - 'a']--;
+            if(occ[c-'a'] < 0)return false;
+        }
+        return true;
+    }
+};
